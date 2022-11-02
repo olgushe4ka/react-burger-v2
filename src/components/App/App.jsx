0cb8e29456316c9412ad2ebef60461efd2bb85d4
olgushe4ka <olgushe4ka@gmail.com>
2022-11-02 14:44:08 +0300

    redux install

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -25,22 +25,20 @@ function App() {
 
   return (
     <div className={appStyles.App}>
-
-        <AppHeader />
-        <div className={`${appStyles.appWithoutHeader}`}>
-          <h1
-            className={`${appStyles.title} text text_type_main-large pl-15 pr-0 pb-5 pt-10`}
-          >
-            Соберите бургер
-          </h1>
-          <div className={`${appStyles.displayFlex}`}>
+      <AppHeader />
+      <div className={`${appStyles.appWithoutHeader}`}>
+        <h1
+          className={`${appStyles.title} text text_type_main-large pl-15 pr-0 pb-5 pt-10`}
+        >
+          Соберите бургер
+        </h1>
+        <div className={`${appStyles.displayFlex}`}>
           <BurgerIngredientsContext.Provider value={ingredients}>
             {ingredients && <BurgerIngredients />}
             {ingredients && <BurgerConstructor />}
-            </BurgerIngredientsContext.Provider>
-          </div>
+          </BurgerIngredientsContext.Provider>
         </div>
-
+      </div>
     </div>
   );
 }
